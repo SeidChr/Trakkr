bb0aba32ce8b8f5987c96451e3e601a3d04550d9
Christian Seidlitz <Christian.Seidlitz@gmx.de>
2016-03-02 20:26:18 +0100

    Added logging and file renaming.

--- Trakkr.Console/Program.cs
+++ Trakkr.Console/Program.cs
@@ -4,7 +4,9 @@ using System.Dynamic;
 using System.Globalization;
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Xml.Linq;
+using JsonFx.Serialization.GraphCycles;
 using Trakkr.Core;
 using Trakkr.YouTrack;
 using YouTrackSharp.Issues;
@@ -20,46 +22,108 @@ namespace Trakkr.Console
             return UnixEpoch.AddMilliseconds(millis);
         }
 
-        public static long GetCurrentUnixTimestampSeconds(DateTime dateTime)
+        public static long GetUnixTimestampMilliseconds(DateTime dateTime)
         {
-            return (long)(dateTime - UnixEpoch).TotalSeconds;
+            return (long)dateTime.Subtract(UnixEpoch).TotalMilliseconds;
         }
 
         static void Main(string[] args)
         {
-            List<TrakkrEntry<string>> entries = new List<TrakkrEntry<string>>();
-            if (string.IsNullOrWhiteSpace(Properties.Settings.Default.Host))
+            if (args[0] == "RESET")
             {
-                System.Console.Write("Youtrack Host: ");
-                var host = System.Console.ReadLine();
-                Properties.Settings.Default.Host = host;
+                ResetSettings();
             }
-
-            if (string.IsNullOrWhiteSpace(Properties.Settings.Default.Username))
+            else
             {
-                System.Console.Write("Username: ");
-                var username = System.Console.ReadLine();
-                Properties.Settings.Default.Username = username;
+                var inputFile = new FileInfo(args[0]);
+
+                var connection = Authenticate();
+
+                var entries = ParseEvents(inputFile);
+
+                var log = UpdateWorkItems(connection, entries);
+                var namePart = Path.Combine(Path.GetDirectoryName(inputFile.FullName), Path.GetFileNameWithoutExtension(inputFile.FullName));
+
+                System.Console.WriteLine("Save log to: " + namePart + ".log");
+
+                inputFile.MoveTo(namePart + ".done");
+                File.WriteAllLines(namePart + ".log", log);
+
+                System.Console.ReadLine();
             }
+        }
 
-            if (string.IsNullOrWhiteSpace(Properties.Settings.Default.Password))
-            {
-                System.Console.Write("Password: ");
-                var password = System.Console.ReadLine();
-                Properties.Settings.Default.Password = password;
-            }
-
-
-            var connection = YouTrackManager.GetConnection(Properties.Settings.Default.Host, Properties.Settings.Default.Username, Properties.Settings.Default.Password);
-
-            if (connection.IsAuthenticated)
-            {
-                Properties.Settings.Default.Save();
-            }
-
+        private static IEnumerable<string> UpdateWorkItems(IConnection connection, List<TrakkrEntry<string>> entries)
+        {
+            var log = new List<string>();
             var issueManagement = new IssueManagement(connection);
+            foreach (var trakkrEntry in entries)
+            {
+                var minutes = (int) Math.Round(trakkrEntry.Duration.TotalMinutes);
+                
+                var issueExists = issueManagement.CheckIfIssueExists(trakkrEntry.Mark);
+                if (issueExists)
+                {
+                    System.Console.Write($"{trakkrEntry.Mark}: {minutes} Minutes. Add (y/n) ? ");
+                    var key = System.Console.ReadKey(false);
+                    System.Console.WriteLine();
 
-            var inputFile = new FileInfo(args[0]);
+                    if (key.Key == ConsoleKey.Y)
+                    {
+                        var doc = "<workItem>"
+                                  + $"<date>{GetUnixTimestampMilliseconds(trakkrEntry.Day)}</date>"
+                                  + $"<duration>{minutes}</duration>"
+                                  + "</workItem>";
+
+                        var response = connection.PostXml($"issue/{trakkrEntry.Mark}/timetracking/workitem", doc);
+                        if (response.StatusCode == HttpStatusCode.Created)
+                        {
+                            System.Console.WriteLine($"Work item saved here: {response.Location}");
+                            log.Add($"Ticket {trakkrEntry.Mark} : {minutes}m : {response.Location}");
+                        }
+
+                        //POST http://localhost:8081/rest/issue/HBR-63/timetracking/workitem
+                    }
+                    else
+                    {
+                        log.Add($"Ticket {trakkrEntry.Mark} : {minutes}m : NOT ADDED!");
+                    }
+
+                    //https://confluence.jetbrains.com/display/YTD6/Get+Available+Work+Items+of+Issue
+
+                    // get all workitems
+                    // GET /rest/issue/{issue}/timetracking/workitem/
+                    //var result = connection.Get<object>($"issue/{trakkrEntry.Mark}/timetracking/workitem/");
+
+                    // add a workitem
+                    //connection.Post($"issue/{trakkrEntry.Mark}/timetracking/workitem", "<xml/>");
+                    //POST http://localhost:8081/rest/issue/HBR-63/timetracking/workitem
+                }
+                else
+                {
+                    log.Add($"Unable to find issue {trakkrEntry.Mark}");
+
+                    //var issues = issueManagement.GetIssuesBySearch(trakkrEntry.Mark);
+                    //foreach (var issue in issues)
+                    //{
+
+                    //    System.Console.WriteLine($"Issue: {issue.Id}");
+                    //    var members = issue.GetDynamicMemberNames();
+                    //    foreach (var member in members)
+                    //    {
+                    //        System.Console.WriteLine($"Member: {member}");
+                    //    }
+                    //}
+                }
+            }
+
+            return log;
+        }
+
+        private static List<TrakkrEntry<string>> ParseEvents(FileInfo inputFile)
+        {
+            List<TrakkrEntry<string>> entries = new List<TrakkrEntry<string>>();
+            
             using (var reader = inputFile.OpenText())
             {
                 // first line is date
@@ -105,57 +169,48 @@ namespace Trakkr.Console
                     }
                 }
             }
+            return entries;
+        }
 
-            foreach (var trakkrEntry in Trakkr<string>.Merge(entries))
+        private static IConnection Authenticate()
+        {
+            if (string.IsNullOrWhiteSpace(Properties.Settings.Default.Host))
             {
-                var minutes = (int) Math.Round(trakkrEntry.Duration.TotalMinutes);
-                System.Console.WriteLine($"{trakkrEntry.Mark}: {minutes} Minutes");
-                var issueExists = issueManagement.CheckIfIssueExists(trakkrEntry.Mark);
-                if (issueExists)
-                {
-                    System.Console.Write($"Found that Issue: {trakkrEntry.Mark}. Add {minutes} minutes to it? Y/N");
-                    var key = System.Console.ReadKey(false);
-                    System.Console.WriteLine();
-                    if (key.Key == ConsoleKey.Y)
-                    {
-                        var doc = "<workItem>"
-                                  + $"<date>{GetCurrentUnixTimestampSeconds(trakkrEntry.Start)}</date>"
-                                  + $"<duration>{minutes}</duration>"
-                                  //+ "<description>first work item</description>"
-                                  + "</workItem>";
-                        connection.Post($"issue/{trakkrEntry.Mark}/timetracking/workitem", doc);
-                        //POST http://localhost:8081/rest/issue/HBR-63/timetracking/workitem
-                    }
-
-                    //https://confluence.jetbrains.com/display/YTD6/Get+Available+Work+Items+of+Issue
-
-                    // get all workitems
-                    // GET /rest/issue/{issue}/timetracking/workitem/
-                    //var result = connection.Get<object>($"issue/{trakkrEntry.Mark}/timetracking/workitem/");
-
-                    // add a workitem
-                    //connection.Post($"issue/{trakkrEntry.Mark}/timetracking/workitem", "<xml/>");
-                    //POST http://localhost:8081/rest/issue/HBR-63/timetracking/workitem
-                }
-                else
-                {
-                    var issues = issueManagement.GetIssuesBySearch(trakkrEntry.Mark);
-
-
-                    foreach (var issue in issues)
-                    {
-                        System.Console.WriteLine($"Issue: {issue.Id}");
-                        var members = issue.GetDynamicMemberNames();
-                        foreach (var member in members)
-                        {
-                            System.Console.WriteLine($"Member: {member}");
-                        }
-
-                    }
-                }
+                System.Console.Write("Youtrack Host: ");
+                var host = System.Console.ReadLine();
+                Properties.Settings.Default.Host = host;
             }
 
-            System.Console.ReadLine();
+            if (string.IsNullOrWhiteSpace(Properties.Settings.Default.Username))
+            {
+                System.Console.Write("Username: ");
+                var username = System.Console.ReadLine();
+                Properties.Settings.Default.Username = username;
+            }
+
+            if (string.IsNullOrWhiteSpace(Properties.Settings.Default.Password))
+            {
+                System.Console.Write("Password: ");
+                var password = System.Console.ReadLine();
+                Properties.Settings.Default.Password = password;
+            }
+
+            var connection = YouTrackManager.GetConnection(Properties.Settings.Default.Host,
+                Properties.Settings.Default.Username, Properties.Settings.Default.Password);
+
+            if (connection.IsAuthenticated)
+            {
+                Properties.Settings.Default.Save();
+            }
+            return connection;
+        }
+
+        private static void ResetSettings()
+        {
+            Properties.Settings.Default.Host = string.Empty;
+            Properties.Settings.Default.Username = string.Empty;
+            Properties.Settings.Default.Password = string.Empty;
+            Properties.Settings.Default.Save();
         }
     }
 }
---
+++ Trakkr.YouTrack/IConnection.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EasyHttp.Http;
+
+namespace Trakkr.YouTrack
+{
+    public interface IConnection : YouTrackSharp.Infrastructure.IConnection
+    {
+        HttpResponse PostXml(string command, string data);
+    }
+}
