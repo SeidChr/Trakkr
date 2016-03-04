d271ae565ef61104b003731d2f779b3639d56958
Christian Seidlitz <Christian.Seidlitz@gmx.de>
2016-03-04 21:19:52 +0100

    added repository interface draft

---
+++ Trakkr.Core/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using Trakkr.Core.Events;
+
+namespace Trakkr.Core
+{
+    public interface IRepository<TId, in TPayload>
+    {
+        IEnumerable<TId> FindTickets(string query);
+         
+        bool HasTicket(TId ticketId);
+
+        bool AddEntryToTicket(TId ticketId, IEntry<TPayload> payload);
+    }
+}
