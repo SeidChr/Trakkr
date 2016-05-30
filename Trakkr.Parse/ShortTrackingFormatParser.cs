0ab326723161cd3f6796b3be3af9833742f79f84
Christian Seidlitz <Christian.Seidlitz@gmx.de>
2016-05-30 15:50:13 +0200

    started with comments

--- Trakkr.Parse/ShortTrackingFormatParser.cs
+++ Trakkr.Parse/ShortTrackingFormatParser.cs
@@ -21,44 +21,66 @@ namespace Trakkr.Parse
                 .SelectMany(ParseDay);
         }
 
+        private static IEnumerable<IEvent<ShortTrackingFormatPayload>> ParseLine(string line)
+        {
+
+        }
+
+        private static IEvent<ShortTrackingFormatPayload> ParseTimeLine(string timeLine, DateTime date)
+        {
+            var timeSplit = timeLine.Split(new[] { ' ' }, 3);
+
+            if (timeSplit.Length < 2)
+            {
+                return null;
+            }
+
+            var time = DateTime.ParseExact(timeSplit[0], "HHmm", null, DateTimeStyles.None).TimeOfDay;
+            var eventTime = date.Add(time);
+            var ticket = timeSplit[1];
+            var description = string.Empty;
+
+            if (timeSplit.Length > 2)
+            {
+                description = timeSplit[2];
+            }
+
+            var isStopEvent = string.Equals(ticket, "stop");
+            return new Event<ShortTrackingFormatPayload>()
+            {
+                Type = isStopEvent ? EventType.Stop : EventType.Start,
+                Timestamp = eventTime,
+                Payload = new ShortTrackingFormatPayload()
+                {
+                    Query = ticket,
+                    Descrition = description,
+                },
+            };
+
+        }
+
         private static IEnumerable<IEvent<ShortTrackingFormatPayload>> ParseDay(string dayString)
         {
             var reader = new StringReader(dayString);
-
+            
             // first line is date
             var dateLine = reader.ReadLine();
             DateTime date;
-            if (DateTime.TryParseExact(dateLine, "yyyy-MM-dd", null, DateTimeStyles.None, out date))
+
+            if (!DateTime.TryParseExact(dateLine, "yyyy-MM-dd", null, DateTimeStyles.None, out date) 
+                && !DateTime.TryParseExact(dateLine, "yyyyMMdd", null, DateTimeStyles.None, out date))
             {
-                date = date.Date;
-                string timeLine;
-                string[] timeSplit;
-                while ((timeLine = reader.ReadLine()) != null)
+                yield break;
+            }
+
+            date = date.Date;
+            string timeLine;
+            while ((timeLine = reader.ReadLine()) != null)
+            {
+                var result = ParseTimeLine(timeLine, date);
+                if (result != null)
                 {
-                    timeSplit = timeLine.Split(new []{' '}, 3);
-                
-                    if (timeSplit.Length >= 2)
-                    {
-                        var time = DateTime.ParseExact(timeSplit[0], "HHmm", null, DateTimeStyles.None).TimeOfDay;
-                        var eventTime = date.Add(time);
-                        var ticket = timeSplit[1];
-                        var description = string.Empty;
-                        if (timeSplit.Length > 2)
-                        {
-                            description = timeSplit[2];
-                        }
-                        var isStopEvent = string.Equals(ticket, "stop");
-                        yield return new Event<ShortTrackingFormatPayload>()
-                        {
-                            Type = isStopEvent ? EventType.Stop : EventType.Start,
-                            Timestamp = eventTime,
-                            Payload = new ShortTrackingFormatPayload()
-                            {
-                                Query = ticket,
-                                Descrition = description,
-                            },
-                        };
-                    }
+                    yield return result;
                 }
             }
         }
